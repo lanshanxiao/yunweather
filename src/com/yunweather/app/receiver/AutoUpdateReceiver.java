76e35d8a071e74efb6bc9cf8d331d03479ae15c8
lanshanxiao <1554378775@qq.com>
2017-07-25 19:56:20 +0800

    First commit.

---
+++ src/com/yunweather/app/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,18 @@
+package com.yunweather.app.receiver;
+
+import com.yunweather.app.service.AutoUpdateService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i = new Intent(context, AutoUpdateService.class);
+		context.startService(i);
+
+	}
+
+}
